2c25903e1fb3ddefcc9afef1f8a769ab8f7fcf72
Ethanil <69218171+Ethanil@users.noreply.github.com>
2024-11-21 21:28:09 +0100

    Add and Use Plant Layer for DetailView

--- Assets/Scripts/PlantManager.cs
+++ Assets/Scripts/PlantManager.cs
@@ -8,23 +8,40 @@ public class PlantManager : MonoBehaviour
     public ObservableCollection<GameObject> Plants = new();
     private HighlightController _highlightController;
     private DetailViewController _detailViewController;
+    private int _plantLayer;
     void Awake()
     {
+        _plantLayer = LayerMask.NameToLayer("Plant");
         _highlightController = FindAnyObjectByType<HighlightController>();
         _detailViewController = FindAnyObjectByType<DetailViewController>();
         //Collect the initial Plants
         foreach (var plantController in gameObject.GetComponentsInChildren<PlantController>())
-        { 
+        {
             Plants.Add(plantController.gameObject);
+            SetLayerOfAllChildren(plantController.gameObject);
         }
         //Make sure that everything is up to date if anything changes
         Plants.CollectionChanged += new NotifyCollectionChangedEventHandler((object sender, NotifyCollectionChangedEventArgs e) => UpdateHighlightAndDetailView());
         UpdateHighlightAndDetailView();
     }
 
+    private void SetLayerOfAllChildren(GameObject gameObject)
+    {
+        gameObject.layer = _plantLayer;
+        foreach (Transform child in gameObject.transform)
+        {
+            SetLayerOfAllChildren(child.gameObject);
+        }
+
+    }
+
     private void UpdateHighlightAndDetailView()
     {
-        _detailViewController.PlantControllers = Plants.Select(plant => plant.GetComponent<PlantController>()).ToList();
+        _detailViewController.PlantControllers = Plants.Select(plant =>
+        {
+            SetLayerOfAllChildren(plant);
+            return plant.GetComponent<PlantController>();
+        }).ToList();
         for (int i = 0; i < Plants.Count; i++)
         {
             ConstructPlantHighlightAndClickFunction(i);
