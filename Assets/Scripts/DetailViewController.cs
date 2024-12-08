12e63a08d849eb51d93d29f21336d16a773a6953
Ethanil <69218171+Ethanil@users.noreply.github.com>
2024-12-08 15:21:00 +0100

    Add Watering function Start refactoring of DetailView by extracting all camera-movements in seperate class

--- Assets/Scripts/DetailViewController.cs
+++ Assets/Scripts/DetailViewController.cs
@@ -6,6 +6,9 @@ using UnityEngine.UIElements;
 public class DetailViewController : MonoBehaviour
 {
 
+    DetailViewCameraController _cameraController;
+
+
     // UI Elements
     public Camera DetailViewCamera;
     private RenderTexture _cameraView;
@@ -22,21 +25,21 @@ public class DetailViewController : MonoBehaviour
 
 
     // Camera Move and Animation
-    private Vector3 _futureCameraPosition = Vector3.zero;
     private Vector3 _rotateVector;
-    private Vector3 _cameraMoveVector;
 
     private Quaternion _savedPlantRotation;
     private Dictionary<int, Quaternion> _plantsToReset = new();
     private void Start()
     {
-        _background = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("background");
+
         //Configure Camera
         var render_texture = _background.Q<Image>("plant-view");
         _cameraView = new RenderTexture(Screen.height, Screen.height, 24);
-        DetailViewCamera.targetTexture = _cameraView;
         render_texture.image = _cameraView;
-        _futureCameraPosition = DetailViewCamera.transform.position;
+        DetailViewCamera.targetTexture = _cameraView;
+        _cameraController = new(DetailViewCamera, DetailViewCamera.transform.position);
+
+        _background = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("background");
 
 
         Label wikiText = _background.Q<Label>("wiki-text");
@@ -53,15 +56,9 @@ public class DetailViewController : MonoBehaviour
     {
         if (_currentPlantControllerIndex < 0)
             return;
-        var unclampedFutureCameraPosition = _futureCameraPosition + _cameraMoveVector * Time.deltaTime;
-        _futureCameraPosition = new Vector3(
-            Mathf.Clamp(unclampedFutureCameraPosition.x, _initialCameraPosition.x - 0.3f, _initialCameraPosition.x + 0.3f),
-            Mathf.Clamp(unclampedFutureCameraPosition.y, _initialCameraPosition.y - 0.3f, _initialCameraPosition.y + 0.3f),
-            Mathf.Clamp(unclampedFutureCameraPosition.z, _initialCameraPosition.z, PlantControllers[_currentPlantControllerIndex].gameObject.transform.position.z));
-        if (_futureCameraPosition != DetailViewCamera.transform.position)
-        {
-            DetailViewCamera.transform.position = Vector3.Lerp(DetailViewCamera.transform.position, _futureCameraPosition, Time.deltaTime * 5);
-        }
+        //NewMethod();
+        _cameraController.Update();
+
         List<int> entriesToRemoveFromPlantsToReset = new();
         foreach (var (plantIndex, savedRotation) in _plantsToReset)
         {
@@ -78,6 +75,8 @@ public class DetailViewController : MonoBehaviour
         }
         PlantControllers[_currentPlantControllerIndex].transform.Rotate(_rotateVector, Space.World);
     }
+
+
     private void AddButtonCallbacks()
     {
         _background.Q<Button>("start-sexing").clicked += () => Debug.Log("totaly starting the sexing minigame");
@@ -97,7 +96,6 @@ public class DetailViewController : MonoBehaviour
         (axis) => _rotateVector += (axis / 2) / (Mathf.Abs(_initialCameraPosition.z / DetailViewCamera.transform.position.z)),
         () => _rotateVector = Vector3.zero);
 
-        _cameraMoveVector = Vector3.zero;
         SetupControlButtons(
             _background.Q<VisualElement>("camera-control-buttons"),
             new Dictionary<string, Vector3> {
@@ -106,8 +104,8 @@ public class DetailViewController : MonoBehaviour
                 { "move-left-button", Vector3.left },
                 { "move-right-button", Vector3.right },
             },
-            (axis) => _cameraMoveVector += (axis / 2) / (Mathf.Abs(_initialCameraPosition.z / DetailViewCamera.transform.position.z)),
-            () => _cameraMoveVector = Vector3.zero);
+            (axis) => _cameraController.SetMovementVector(axis / 2),
+            () => _cameraController.SetMovementVector(Vector3.zero));
 
         SetupControlButtons(
         _background.Q<VisualElement>("zoom-control-buttons"),
@@ -115,8 +113,8 @@ public class DetailViewController : MonoBehaviour
                     { "zoom-in-button", Vector3.forward },
                     { "zoom-out-button", Vector3.back},
                 },
-        (axis) => _cameraMoveVector += axis,
-        () => _cameraMoveVector = Vector3.zero);
+            (axis) => _cameraController.SetMovementVector(axis),
+            () => _cameraController.SetMovementVector(Vector3.zero));
 
         _previousPlantButton = _background.Q<Button>("previous-plant-button");
         _previousPlantButton.clicked += () => ShowOtherPlant(() => _currentPlantControllerIndex--);
@@ -124,8 +122,66 @@ public class DetailViewController : MonoBehaviour
         _nextPlantButton = _background.Q<Button>("next-plant-button");
         _nextPlantButton.clicked += () => ShowOtherPlant(() => _currentPlantControllerIndex++);
         AddClickSound(_nextPlantButton);
+
+        var waterButton = _background.Q<Button>("open-watering-submenu-button");
+        _submenues = _background.Q<VisualElement>("submenues");
+        _wateringSubmenu = _submenues.Q<VisualElement>("watering-submenu");
+        _waterSlider = _wateringSubmenu.Q<Slider>("water-slider");
+        _fertilizerSlider = _wateringSubmenu.Q<Slider>("fertilizer-slider");
+        waterButton.clicked += () => {
+            _submenues.style.display = DisplayStyle.Flex;
+            _wateringSubmenu.style.display = DisplayStyle.Flex;
+            _waterSlider.value = 10;
+            _fertilizerSlider.value = 0;
+        };
+        AddClickSound(waterButton);
+        var closeWateringSubmenuButton = _wateringSubmenu.Q<Button>("close-watering-submenu-button");
+        closeWateringSubmenuButton.clicked += CloseWaterSubmenu;
+        AddClickSound(closeWateringSubmenuButton);
+        var waterPlantsButton = _wateringSubmenu.Q<Button>("water-plant-button");
+        waterPlantsButton.clicked += WaterCurrentPlant;
+        waterPlantsButton.clicked += CloseWaterSubmenu;
+        AddClickSound(waterPlantsButton);
+        var potsizeButton = _background.Q<Button>("open-change-pot-submenu-button");
+        potsizeButton.clicked += ChangePotOfCurrentPlant;
+        AddClickSound(potsizeButton);
     }
 
+    Slider _waterSlider;
+    Slider _fertilizerSlider;
+    VisualElement _submenues;
+    VisualElement _wateringSubmenu;
+    private void ChangePotOfCurrentPlant()
+    {
+        var currentPotsize = PlantControllers[_currentPlantControllerIndex].Potsize;
+        switch (currentPotsize)
+        {
+            case Potsize.Cultivation:
+                PlantControllers[_currentPlantControllerIndex].Potsize = Potsize.Small;
+                break;
+            case Potsize.Small:
+                PlantControllers[_currentPlantControllerIndex].Potsize = Potsize.Medium;
+                break;
+            case Potsize.Medium:
+                PlantControllers[_currentPlantControllerIndex].Potsize = Potsize.Large;
+                break;
+            case Potsize.Large:
+                break;
+        }
+        FillDataLabel();
+    }
+
+    private void WaterCurrentPlant()
+    {
+        PlantControllers[_currentPlantControllerIndex].Soil.StoredWater += _waterSlider.value;
+        PlantControllers[_currentPlantControllerIndex].Soil.StoredNutrients += _fertilizerSlider.value;
+        FillDataLabel();
+    }
+    private void CloseWaterSubmenu()
+    {
+        _submenues.style.display = DisplayStyle.None;
+        _wateringSubmenu.style.display = DisplayStyle.None;
+    }
     void SetupControlButtons(VisualElement container, Dictionary<string, Vector3> buttonMappings, Action<Vector3> onPointerDown, Action onPointerUp)
     {
         if (container == null)
@@ -166,7 +222,6 @@ public class DetailViewController : MonoBehaviour
         _closeAction = closeAction;
         SafeTransform();
         ShowOtherPlant(() => _currentPlantControllerIndex = plantControllerIndex);
-        DetailViewCamera.transform.position = _futureCameraPosition;
     }
 
     public void DisableView()
@@ -192,8 +247,12 @@ public class DetailViewController : MonoBehaviour
     private Vector3 _initialCameraPosition;
     private void PopulateData()
     {
-        _futureCameraPosition = PlantControllers[_currentPlantControllerIndex].gameObject.transform.position + _cameraOffsetVector;
-        _initialCameraPosition = _futureCameraPosition;
+        _cameraController.SetTarget(PlantControllers[_currentPlantControllerIndex].gameObject.transform.position);
+        FillDataLabel();
+    }
+
+    private void FillDataLabel()
+    {
         var data = PlantControllers[_currentPlantControllerIndex].DataDictionary();
         foreach (var (name, label) in _labelsWithName)
         {
@@ -202,6 +261,69 @@ public class DetailViewController : MonoBehaviour
             label.text = data[name].ToString();
         }
     }
+}
+
+public class  DetailViewCameraController
+{
+    private Camera _camera;
+    private Vector3 _futurePosition;
+    private Vector3 _plantPositionToTarget;
+    private Vector3 _cameraOffsetVector = new(0, 0.75f, -5); //Vector3 cant be const
+    private Vector3 _cameraMovementVector = Vector3.zero;
+    public DetailViewCameraController(Camera camera, Vector3 futurePosition)
+    {
+        _camera = camera;
+        _futurePosition = futurePosition;
+        _plantPositionToTarget = futurePosition;
+    }
+
+
+    public void SetTarget(Vector3 plantPositionToTarget)
+    {
+        var target = plantPositionToTarget + _cameraOffsetVector;
+        _futurePosition = target;
+        _plantPositionToTarget = plantPositionToTarget;
+    }
+
+
+    public void SetMovementVector(Vector3 cameraMovementVector)
+    {
+        // slow down the camera movement when we are near the plant
+        var slowDown = Mathf.Abs((_plantPositionToTarget + _cameraOffsetVector).z / _camera.transform.position.z);
+        const float maximumSlowdown = 0.3f;
+        var x = cameraMovementVector.x * Mathf.Clamp(slowDown, maximumSlowdown, 1.0f);
+        var y = cameraMovementVector.y * Mathf.Clamp(slowDown, maximumSlowdown, 1.0f);
+        var z = cameraMovementVector.z;
+        _cameraMovementVector = new Vector3(x,y,z);
+    }
+
+    public void Update()
+    {
+        // make sure that the camera is near the actual plant
+        var unclampedFutureCameraPosition = _futurePosition + _cameraMovementVector * Time.deltaTime;
+        var initialCameraPosition = _plantPositionToTarget + _cameraOffsetVector;
+        _futurePosition = new Vector3(
+            Mathf.Clamp(unclampedFutureCameraPosition.x, initialCameraPosition.x - 0.3f, initialCameraPosition.x + 0.3f),
+            Mathf.Clamp(unclampedFutureCameraPosition.y, initialCameraPosition.y - 0.3f, initialCameraPosition.y + 0.3f),
+            Mathf.Clamp(unclampedFutureCameraPosition.z, initialCameraPosition.z, _plantPositionToTarget.z));
+        if (_futurePosition != _camera.transform.position)
+        {
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, _futurePosition, Time.deltaTime * 5);
+        }
+    }
+}
+
+
+
+public class DetailViewUIManager
+{
+
+}
+
+
+
+public class DetailViewPlantManager
+{
 
 }
 
