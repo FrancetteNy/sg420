40ff02b59ed6915af0a2b22488654bb253e044a0
Ethanil <69218171+Ethanil@users.noreply.github.com>
2024-12-10 18:54:19 +0100

    Move UI Scripts into own Folder Add Serializable GameState Connect HUD with this Gamestate

--- Assets/Scripts/UI/HUDView.cs
+++ Assets/Scripts/UI/HUDView.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 
 public class HUDView : UIView
 {
+    HUDController _controller;
     public HUDView(VisualElement root, UIManager manager): base(root, manager)
     {
     }
@@ -19,6 +20,9 @@ public class HUDView : UIView
         }
         Asset.CloneTree(Root);
         View = Root.Q<VisualElement>("HUD");
+
+        _controller = Manager.gameObject.AddComponent<HUDController>();
+        _controller.Initialize(View);
     }
     public override void Dispose()
     {
@@ -28,10 +32,12 @@ public class HUDView : UIView
     public override void Hide()
     {
         View.style.display = DisplayStyle.None;
+        _controller.enabled = false;
     }
 
     public override void Show()
     {
         View.style.display = DisplayStyle.Flex;
+        _controller.enabled = true;
     }
 }
