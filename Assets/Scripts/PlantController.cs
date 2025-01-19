cae5fc00ad721f31a739b1a4e20da3ab46d3f39f
Nyepgan Yonmba Francette Joss <joss@MacBook-Pro-von-Nyepgan-2.local>
2025-01-19 19:16:38 +0100

    FUNKTIONALITÄT FÜR DEN WECHSEL DES KULTIVIERUNGSZUSTANDS

--- Assets/Scripts/PlantController.cs
+++ Assets/Scripts/PlantController.cs
@@ -6,17 +6,18 @@ public class PlantController : MonoBehaviour
     public PlantData PlantData;
 
     public bool IsEmpty()
-{
-    return PlantData == null || PlantData.Strain == Strain.None;
-}
-
-public void PlantSeed(Strain strain)
-{
-    if (PlantData == null)
     {
-        PlantData = new PlantData();
+        return PlantData == null || PlantData.Strain == Strain.None;
     }
-    PlantData.Strain = strain;
-}
+
+    public void PlantSeed(Strain strain)
+    {
+        if (PlantData == null)
+        {
+            PlantData = new PlantData();
+        }
+        PlantData.Strain = strain;
+        PlantData.Age.Stage = Age.GrowthStage.GERMINATION;
+    }
 
 }
