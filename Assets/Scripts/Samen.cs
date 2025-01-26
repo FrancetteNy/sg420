1aa2a806c61f2cb44f2e9ce188f12180eb5843cc
Nyepgan Yonmba Francette Joss <francette.nyepgan@stud.tu-darmstadt.de>
2025-01-26 15:05:37 +0100

    in PlnatController change IsEmpty to IsPlantable

--- Assets/Scripts/Samen.cs
+++ Assets/Scripts/Samen.cs
@@ -3,11 +3,5 @@ using UnityEngine;
 
 public class Samen : Inventory
 {
-    // Propriété ou méthode spécifique à Samen
 
-    public void AddSeed(string seedName, int quantity)
-    {
-        AddItem(seedName, quantity);
-        Debug.Log($"{quantity} graines de {seedName} ajoutées à l'inventaire.");
-    }
 }
