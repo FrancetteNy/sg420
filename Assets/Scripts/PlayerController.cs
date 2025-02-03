9a2c7c9bc64dafcff7b4649068233d7c526380ef
Fatima Mansour <fatima_zahra.mansour@stud.tu-darmstadt.de>
2025-02-03 15:06:06 +0100

    change in PlayerController SampleScene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public static PlayerController Instance;
     InputSystem_Actions _playerInput;
 
     public Action GoingToDryingRoomAction;
     public Action GoingToMainRoomAction;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    private static PlayerController _instance;
+    public static PlayerController Instance
     {
-        Instance = this;
+        get
+        {
+            if (!_instance)
+            {
+                // NOTE: read docs to see directory requirements for Resources.Load!
+                var prefab = Resources.Load<GameObject>("PlayerController");
+                // create the prefab in your scene
+                var inScene = Instantiate<GameObject>(prefab);
+                // try find the instance inside the prefab
+                _instance = inScene.GetComponentInChildren<PlayerController>();
+                // guess there isn't one, add one
+                if (!_instance)
+                    _instance = inScene.AddComponent<PlayerController>();
+                // mark root as DontDestroyOnLoad();
+                DontDestroyOnLoad(_instance.transform.root.gameObject);
+                _instance._playerInput = new InputSystem_Actions();
+                _instance._playerInput.Enable();
 
-        _playerInput = new InputSystem_Actions();
-        _playerInput.Enable();
+                _instance._playerInput.Player.MoveToDryingRoom.performed += _instance.MoveToDryingRoom_performed;
+                _instance._playerInput.Player.MoveToMainRoom.performed += _instance.MoveToMainRoom_performed;
 
-        _playerInput.Player.MoveToDryingRoom.performed += MoveToDryingRoom_performed;
-        _playerInput.Player.MoveToMainRoom.performed += MoveToMainRoom_performed;
+            }
+            return _instance;
+        }
     }
 
     private void MoveToMainRoom_performed(InputAction.CallbackContext obj)
