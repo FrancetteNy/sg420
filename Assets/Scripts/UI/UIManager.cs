465219147280c53bef4a06cde1c3d773c9b263ae
Ethanil <69218171+Ethanil@users.noreply.github.com>
2025-02-20 13:34:17 +0100

    Add Reinitializiation for NotificationManager after load

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -29,6 +29,7 @@ public class UIManager : MonoBehaviour
         AddAllUIViews();
         SetupNotificationMananger();
         SetupActionSystem();
+        NotificationManagerSingleton.Instance.ReInitializeAfterLoad();
     }
 
     private void SetupActionSystem()
@@ -37,7 +38,6 @@ public class UIManager : MonoBehaviour
         _actions.UI.Enable();
         _actions.UI.Cancel.performed += OnCancelPerformed;
     }
-
     private void OnCancelPerformed(InputAction.CallbackContext context)
     {
         _currentView?.OnCancelPerformed(context);
@@ -123,8 +123,9 @@ public class UIManager : MonoBehaviour
         UIEvents.HideMainMenuView -= OnHudShown;
         _mainMenuView.Dispose();
 
-
+        _actions.UI.Disable();
         _actions.UI.Cancel.performed -= OnCancelPerformed;
+        UIEvents.AddNotification -= _notificationManager.AddNotification;
     }
 
     private void ShowView(UIView view, int? index = null)
